eb45d4352797f535d0350a1401f81fcdb540280a
Peniel Nissi Obeng <p.obeng@alustudent.com>
2025-06-19 14:51:22 +0200

    extra addition to the copilot

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-
 read -p "Enter assignment name: " assName
 
-source ./create_environment.sh
+#In all directories that have the constant name submission_reminder_, repklace the value of ASSIGNMENT in the config file with the user's input.
+for i in submission_reminder_*/config; do
+    if [ -f "$i/config.env" ]; then
+        sed -i "2s/.*/ASSIGNMENT=\"$assName\"/" "$i/config.env"
+        echo "ASSIGNMENT in $i/config.env has been updated to $assName"
+    fi
+done
 
-sed -i "2s/.*/ASSIGNMENT=\"$assName\"/" $dir_name/config/config.env
-
-./$dir_name/startup.sh
+# In all directories that have the constant name submission_reminder_, re-run the startup script.
+for j in submission_reminder_*/; do
+    if [ -f "$j/startup.sh" ]; then
+        cd "$j"
+        ./startup.sh
+        cd ..
+    fi
+done
